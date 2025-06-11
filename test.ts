99970a22a1a137a69e0c6da117fb314a78399f56
nathanielyoon <nathanielyoon1@gmail.com>
2025-06-10 23:52:40 -0400

    fix: destructure

--- test.ts
+++ test.ts
@@ -6,8 +6,8 @@ Deno.test("wycheproof", () =>
   fetch(
     "https://raw.githubusercontent.com/C2SP/wycheproof/f3071a24598bd87d09cd592dbe9fe2516d385590/testvectors_v1/xchacha20_poly1305_test.json",
   ).then<{ testGroups: { tests: Record<string, string>[] }[] }>(($) => $.json())
-    .then(($) =>
-      $.testGroups.forEach(({ tests }) =>
+    .then(({ testGroups }) =>
+      testGroups.forEach(({ tests }) =>
         tests.forEach(($) => {
           const a = ["key", "iv", "msg", "aad"].map((key) => hex($[key]));
           const b = hex($.ct + $.tag);
